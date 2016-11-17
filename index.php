e15d2b38dbfb4513cb57249a90f57a0c47ea9396
nicolas.preveraud <nicolas.preveraud@gmail.com>
2016-11-17 10:25:36 +0100

    save4

---
+++ index.php
@@ -0,0 +1,23 @@
+﻿<?php
+require_once("include/inc_fonction.php");
+require_once("include/inc_pdo_amis.php");
+
+session_start();
+$pdo = new Pdo_amis();
+
+$controleur = $_REQUEST['controleur'];
+switch($controleur){
+	
+	case 'c_action' : {
+		include("controleur/c_action.php");
+		break;
+	}
+	
+	case 'c_commission' : {
+		include("controleur/c_commission.php");
+		break;
+	}
+
+}
+
+?>
